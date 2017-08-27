af58d06f055874877ebf4646e5a2ce35dd34c45b
Suchir Balaji <suchirbalaji@gmail.com>
2017-08-27 10:35:08 -0700

    fix change_directory

--- caching.py
+++ caching.py
@@ -3,18 +3,23 @@ import os
 import hashlib
 
 
+ROOT_DIR = os.getcwd()
+
+
 @contextlib.contextmanager
 def change_directory(dir):
+    dir = f'{ROOT_DIR}\\{dir}'
     if not os.path.exists(dir):
         os.makedirs(dir)
-    init = os.getcwd()
+
+    init_dir = os.getcwd()
     os.chdir(dir)
     yield
-    os.chdir(init)
+    os.chdir(init_dir)
 
 
 def read_input_dir(dir=''):
-    return change_directory(f'input/{dir}')
+    return change_directory(f'input\\{dir}')
 
 
 class CachedFunction(object):
@@ -22,12 +27,19 @@ class CachedFunction(object):
         self.version = version
         self.ancestors = set.union({self}, *(x.ancestors for x in deps))
         self.version = sum(x.version for x in self.ancestors)
-        self.dirname = f'{fn.__name__}-{self.version}'
+        self.fn_name = fn.__name__
+        self.dirname = f'{self.fn_name}-{self.version}'
+        self.cache = {}
         self._fn = fn
 
     def __call__(self, mode):
-        with change_directory(f'cache/{self.dirname}-{mode}'):
-            return self._fn(mode)
+        if mode not in self.cache:
+            print(f'running {self.fn_name}-{mode}... ')
+            with change_directory(f'cache\\{self.dirname}-{mode}'):
+                self.cache[mode] = self._fn(mode)
+            print(f'{self.fn_name}-{mode} completed')
+
+        return self.cache[mode]
 
 
 def cached(*deps, version=0):
