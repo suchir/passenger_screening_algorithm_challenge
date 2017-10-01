7cbfbc2eee6c75d593eaabe879c28f68d880305f
Suchir Balaji <suchirbalaji@gmail.com>
2017-10-01 16:57:29 -0700

    move get_data to common

--- a3daps/dataio.py
+++ a3daps/dataio.py
@@ -1,5 +1,5 @@
 from common.caching import read_input_dir, cached
-from common.dataio import read_data, get_train_labels, write_answer_csv
+from common.dataio import read_data, get_train_labels, write_answer_csv, get_data
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,51 +9,6 @@ import os
 import tqdm
 import h5py
 import pickle
-
-
-def get_data(mode):
-    assert mode in ('sample', 'sample_large', 'all', 'sample_train', 'train', 'sample_valid',
-                    'valid', 'sample_test', 'test')
-    with read_input_dir('competition_data/a3daps'):
-        files = glob.glob('*.a3daps')
-
-    labels = get_train_labels()
-    has_label = lambda file: file.split('.')[0] in labels
-    if mode.endswith('test'):
-        files = [file for file in files if not has_label(file)]
-    else:
-        files = [file for file in files if has_label(file)]
-        if mode.endswith('train'):
-            files = files[100:]
-        elif mode.endswith('valid'):
-            files = files[:100]
-    if mode.startswith('sample'):
-        if mode.endswith('large'):
-            files = files[:100]
-        else:
-            files = files[:10]
-
-    with read_input_dir('competition_data/a3daps'):
-        files = [os.path.abspath(file) for file in files]
-
-    def generator():
-        for file in tqdm.tqdm(files):
-            file = file.replace('\\', '/')
-            name = file.split('/')[-1].split('.')[0]
-            yield name, labels.get(name), read_data(file)
-
-    class DataGenerator(object):
-        def __init__(self):
-            self.gen = generator()
-            self.len = len(files)
-
-        def __len__(self):
-            return self.len
-
-        def __iter__(self):
-            return self.gen
-
-    return DataGenerator()
 
 
 @cached(version=3, subdir='ssd')
--- common/dataio.py
+++ common/dataio.py
@@ -1,4 +1,4 @@
-from common.caching import read_input_dir
+from common.caching import read_input_dir, cached
 
 import numpy as np
 import os
@@ -129,6 +129,53 @@ def read_data(infile):
         return data
     else:
         return real, imag
+
+
+@cached(version=0)
+def get_data(mode, dtype):
+    assert mode in ('sample', 'sample_large', 'all', 'sample_train', 'train', 'sample_valid',
+                    'valid', 'sample_test', 'test')
+    assert dtype in ('aps', 'a3daps', 'a3d')
+    with read_input_dir('competition_data/%s' % dtype):
+        files = glob.glob('*')
+
+    labels = get_train_labels()
+    has_label = lambda file: file.split('.')[0] in labels
+    if mode.endswith('test'):
+        files = [file for file in files if not has_label(file)]
+    else:
+        files = [file for file in files if has_label(file)]
+        if mode.endswith('train'):
+            files = files[100:]
+        elif mode.endswith('valid'):
+            files = files[:100]
+    if mode.startswith('sample'):
+        if mode.endswith('large'):
+            files = files[:100]
+        else:
+            files = files[:10]
+
+    with read_input_dir('competition_data/%s' % dtype):
+        files = [os.path.abspath(file) for file in files]
+
+    def generator():
+        for file in tqdm.tqdm(files):
+            file = file.replace('\\', '/')
+            name = file.split('/')[-1].split('.')[0]
+            yield name, labels.get(name), read_data(file)
+
+    class DataGenerator(object):
+        def __init__(self):
+            self.gen = generator()
+            self.len = len(files)
+
+        def __len__(self):
+            return self.len
+
+        def __iter__(self):
+            return self.gen
+
+    return DataGenerator()
 
 
 def get_train_labels():
