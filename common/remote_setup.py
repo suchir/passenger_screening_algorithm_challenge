e1882eb475a5d11e380275ed1a18b93911d3ec7e
Suchir Balaji <suchirbalaji@gmail.com>
2017-10-27 20:03:44 -0700

    add setup script and requirements.txt

---
+++ common/remote_setup.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+import os
+
+
+def download_bucket(path, bucket):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    subprocess.check_call(['gsutil', '-m', 'cp', '-r', 'gs://%s' % bucket, path])
+
+
+if __name__ == '__main__':
+    inputs = set(sys.argv[1:])
+    assert inputs <= {'aps', 'a3daps', 'a3d', 'hand_labeling'}, "unrecognized input"
+
+    for dtype in ('aps', 'a3daps', 'a3d'):
+        if dtype in inputs:
+            download_bucket('input/competition_data', 'kaggle-tsa-stage1/stage1/%s' % dtype)
+    if 'hand_labeling' in inputs:
+        download_bucket('input', 'psac_hand_labeling/hand_labeling')
+    if inputs:
+        download_bucket('input/competition_data', 'psac_competition_data/*')
+
+    subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
