0e1ed3ebcea67523e0ae72b95f3fc67efc438737
Suchir Balaji <suchirbalaji@gmail.com>
2017-10-31 22:14:17 -0700

    fix refine2d

--- model_v2/threat_segmentation_models.py
+++ model_v2/threat_segmentation_models.py
@@ -48,9 +48,9 @@ def train_unet_cnn(mode, batch_size, learning_rate, duration, rotate_images=Fals
 
     if refine2d or refine3d:
         if refine2d:
-            logits = tf.concat([tf.transpose(logits, [15]+list(range(15))),
+            logits = tf.concat([tf.concat([logits[-1], logits[:-1]], axis=0),
                                 logits,
-                                tf.transpose(logits, list(range(1, 16))+[0])],
+                                tf.concat([logits[:-1], logits[-1]], axis=0)],
                                axis=-1)
         logits = tf_models.unet_cnn(logits, 4, width, 8, conv3d=refine3d)
         pred_hmap = tf.squeeze(tf.image.resize_images(tf.sigmoid(logits), (height, width)))
