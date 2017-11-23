fix data normalization

model_v2/body_zone_segmentation.py
@@ -232,7 +232,7 @@ def get_depth_maps(mode):
 
 
 @cached(synthetic_data.render_synthetic_zone_data, get_depth_maps, cloud_cache=True, subdir='ssd',
-        version=3)
+        version=4)
 def get_normalized_synthetic_zone_data(mode):
     if not os.path.exists('done'):
         _, _, dset_in = get_depth_maps(mode)
@@ -283,7 +283,7 @@ def get_normalized_synthetic_zone_data(mode):
                 hz, wz = (h-distr[0, 0])/distr[0, 1], (w-distr[1, 0])/distr[1, 1]
                 hp, wp = hz*distr_in[0, 1]+distr_in[0, 0], wz*distr_in[1, 1]+distr_in[1, 0]
                 resized = skimage.transform.resize(crop, (min(330, int(hp)), min(256, int(wp))),
-                                                   preserve_range=True)
+                                                   preserve_range=True, order=0)
                 resized = resized[1:-1, 1:-1]
                 h_pad, w_pad = 330-resized.shape[0], (256-resized.shape[1])//2
                 normal = np.stack([